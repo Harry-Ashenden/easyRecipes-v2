f57fc685d3cd1ac3a7729a4a36a3f74ec7021b01
Harry-Ashenden <harryashenden752@gmail.com>
2026-03-26 01:00:56 +0000

    fixed global error handler

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
 });
 
 // Global Error Handler
-app.use((err, res) => {
+app.use((err, req, res, next) => {
   console.error('Error:', err);
   res.status(err.status || 500).json({
     error: err.message || 'Internal Server Error',
